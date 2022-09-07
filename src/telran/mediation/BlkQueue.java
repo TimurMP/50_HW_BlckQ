44a3eb366b7640fe543652d41bf753461a7c5971
TimurMP <titimurmamyshev@gmail.commur>
2022-09-07 16:37:38 +0300

    In Progress 1.0

--- src/telran/mediation/BlkQueue.java
+++ src/telran/mediation/BlkQueue.java
@@ -1,21 +1,74 @@
 package telran.mediation;
 
-public class BlkQueue<T> implements IBlkQueue<T> {
+import java.util.LinkedList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-	public BlkQueue(int maxSize) {
-		// TODO
-		throw new UnsupportedOperationException("Not implemented");
+public class BlkQueue<T> implements IBlkQueue<T> {
+	LinkedList<T> messages = new LinkedList<>();
+	int buffer;
+	Lock mutex = new ReentrantLock();
+	Condition producerWaitingCondition = mutex.newCondition();
+	Condition consumerWaitingCondition = mutex.newCondition();
+
+
+
+		public BlkQueue(int maxSize) {
+			this.buffer = maxSize;
+//		throw new UnsupportedOperationException("Not implemented");
 	}
 
 	@Override
 	public void push(T message) {
-		// TODO
-		throw new UnsupportedOperationException("Not implemented");
+		mutex.lock();
+		try {
+			if (messages.size() < buffer){
+				messages.addLast(message);
+			}
+			else {
+				while (messages.size() == buffer){
+					try {
+						producerWaitingCondition.await();
+					} catch (InterruptedException e) {
+						throw new RuntimeException(e);
+					}
+				}
+				messages.add(message);
+				consumerWaitingCondition.signal();
+			}
+		} finally {
+			mutex.unlock();
+		}
 	}
 
 	@Override
 	public T pop() {
-		// TODO
-		throw new UnsupportedOperationException("Not implemented");
+		T res;
+		mutex.lock();
+
+
+		try {
+			if (!messages.isEmpty()){
+				res = messages.removeLast();
+			}
+			else {
+				while (messages.isEmpty()){
+					try {
+						consumerWaitingCondition.await();
+					} catch (InterruptedException e) {
+						throw new RuntimeException(e);
+					}
+				}
+				res = messages.removeLast();
+				producerWaitingCondition.signal();
+				return res;
+
+			}
+		} finally {
+			mutex.unlock();
+		}
+		return null;
+
 	}
 }
